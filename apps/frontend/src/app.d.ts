ec6f88a1bc3e4e31ab88d69c4cfb6b1065646bac
YoDDV <116607353+Yo-DDV@users.noreply.github.com>
2026-07-24 23:42:44 +0200

    fix(frontend): synchronize notification sidebar navigation (#189)

--- apps/frontend/src/app.d.ts
+++ apps/frontend/src/app.d.ts
@@ -6,6 +6,8 @@ declare global {
     // interface Locals {}
     // interface PageData {}
     interface PageState {
+      notificationServerId?: string;
+      notificationSidebarOpen?: boolean;
       threadFilter?: 'all' | 'unread';
       welcome?: boolean;
       modal?: {
