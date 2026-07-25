f9d2fe39a919ac341603b262061871b7a66c00ac
YoDDV <116607353+Yo-DDV@users.noreply.github.com>
2026-07-25 14:18:28 +0200

    test(frontend): validate KLIPY URL shapes

--- apps/frontend/src/lib/externalGif.spec.ts
+++ apps/frontend/src/lib/externalGif.spec.ts
@@ -13,10 +13,10 @@ const validURLs: ReadonlyArray<
   readonly [string, ExternalGifProvider, ExternalGifRenderMode, string]
 > = [
   [
-    'https://giphy.com/gifs/reaction-happy-l0MYt5jPR6QX5pnqM',
+    'https://giphy.com/gifs/justin-word-oh-really-wow-QUENDfi6DEMLzQ0CKt',
     'giphy',
     'iframe',
-    'https://giphy.com/embed/l0MYt5jPR6QX5pnqM'
+    'https://giphy.com/embed/QUENDfi6DEMLzQ0CKt'
   ],
   [
     'https://giphy.com/gifs/reaction-happy-l0MYt5jPR6QX5pnqM/',
@@ -43,22 +43,10 @@ const validURLs: ReadonlyArray<
     'https://media.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif?cid=test'
   ],
   [
-    'https://media0.giphy.com/media/l0MYt5jPR6QX5pnqM/200w.gif',
-    'giphy',
-    'image',
-    'https://media0.giphy.com/media/l0MYt5jPR6QX5pnqM/200w.gif'
-  ],
-  [
     'https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjEx/l0MYt5jPR6QX5pnqM/giphy.webp',
     'giphy',
     'image',
     'https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjEx/l0MYt5jPR6QX5pnqM/giphy.webp'
-  ],
-  [
-    'https://i.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif',
-    'giphy',
-    'image',
-    'https://i.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif'
   ],
   [
     'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjEx/l0MYt5jPR6QX5pnqM/giphy.gif',
@@ -85,95 +73,61 @@ const validURLs: ReadonlyArray<
     'https://media1.tenor.com/m/2wdlar795ZAAAAAd/example-content-url.gif'
   ],
   [
-    'https://media1.tenor.com/images/1169d1ab96669e13062c1b23ce5b9b01/tenor.gif?itemid=123',
-    'tenor',
-    'image',
-    'https://media1.tenor.com/images/1169d1ab96669e13062c1b23ce5b9b01/tenor.gif?itemid=123'
-  ],
-  [
-    'https://media.tenor.com/images/36dfe91d9753a9e45a9ed316b83db346/tenor.webp',
-    'tenor',
-    'image',
-    'https://media.tenor.com/images/36dfe91d9753a9e45a9ed316b83db346/tenor.webp'
-  ],
-  [
-    'https://media.tenor.com/AbCdEfGhIjK/tenor.gif',
-    'tenor',
-    'image',
-    'https://media.tenor.com/AbCdEfGhIjK/tenor.gif'
-  ],
-  [
     'https://media.tenor.com/m/AbCdEfGhIjK/AAAAC/tenor.mp4',
     'tenor',
     'video',
     'https://media.tenor.com/m/AbCdEfGhIjK/AAAAC/tenor.mp4'
   ],
   [
-    'https://c.tenor.com/AbCdEfGhIjK/tenor.webp',
+    'https://c.tenor.com/images/0123456789abcdef0123456789ABCDEF/tenor.webp',
     'tenor',
     'image',
-    'https://c.tenor.com/AbCdEfGhIjK/tenor.webp'
+    'https://c.tenor.com/images/0123456789abcdef0123456789ABCDEF/tenor.webp'
   ],
   [
-    'https://media.tenor.com/m/AbCdEfGhIjK/reaction-video.webm',
-    'tenor',
+    'https://static.klipy.com/ii/4493325008d34b7bf8cd6813cd5c1619/12/66/VRmb0agTs8UFUzia.gif',
+    'klipy',
+    'image',
+    'https://static.klipy.com/ii/4493325008d34b7bf8cd6813cd5c1619/12/66/VRmb0agTs8UFUzia.gif'
+  ],
+  [
+    'https://static.klipy.co/ii/3bbfac09dcb32c2b1e87ad063c4ac16e/9d/55/7VnHqCsL.webp',
+    'klipy',
+    'image',
+    'https://static.klipy.co/ii/3bbfac09dcb32c2b1e87ad063c4ac16e/9d/55/7VnHqCsL.webp'
+  ],
+  [
+    'https://static.klipy.com/ii/935d7ab9d8c6202580a668421940ec81/bc/1d/hYDJ4v1I.mp4?download=0',
+    'klipy',
     'video',
-    'https://media.tenor.com/m/AbCdEfGhIjK/reaction-video.webm'
+    'https://static.klipy.com/ii/935d7ab9d8c6202580a668421940ec81/bc/1d/hYDJ4v1I.mp4?download=0'
   ]
 ];
 
 const invalidURLs = [
   'http://giphy.com/gifs/test-l0MYt5jPR6QX5pnqM',
   'https://user@giphy.com/gifs/test-l0MYt5jPR6QX5pnqM',
-  'https://giphy.com:444/gifs/test-l0MYt5jPR6QX5pnqM',
   'https://giphy.com:443/embed/l0MYt5jPR6QX5pnqM',
-  'https://giphy.com:/embed/l0MYt5jPR6QX5pnqM',
-  'https://giphy.com:0443/embed/l0MYt5jPR6QX5pnqM',
-  'https://@giphy.com/embed/l0MYt5jPR6QX5pnqM',
-  'https://giphy.com/embed/l0MYt5jPR6QX5pnqM\n',
-  'https://giphy.com/embed/l0MYt5jPR6QX5pnqM?label=réaction',
   'https://giphy.com.evil.example/gifs/test-l0MYt5jPR6QX5pnqM',
-  'https://evil.example/media/l0MYt5jPR6QX5pnqM/giphy.gif',
   'https://media5.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif',
-  'https://media999.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif',
   'https://media.giphy.com/media/%2Fetc/giphy.gif',
-  'https://media.giphy.com/media/%6c0MYt5jPR6QX5pnqM/giphy.gif',
-  'https://giphy.com/%2e/embed/l0MYt5jPR6QX5pnqM',
-  'https://giphy.com/%2e%2e/gifs/reaction-l0MYt5jPR6QX5pnqM',
-  'https://giphy.com/gifs/%2e%2e/embed/l0MYt5jPR6QX5pnqM',
-  'https://giphy.com/./embed/l0MYt5jPR6QX5pnqM',
-  'https://giphy.com/gifs/../embed/l0MYt5jPR6QX5pnqM',
-  'https://media.giphy.com/media/./l0MYt5jPR6QX5pnqM/giphy.gif',
-  'https://media.giphy.com/media/v1/../l0MYt5jPR6QX5pnqM/giphy.gif',
   'https://media.giphy.com/media/l0MYt5jPR6QX5pnqM//giphy.gif',
   'https://media.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif/',
-  'https://media.giphy.com/media/short/giphy.gif',
-  'https://media.giphy.com/media/v1..bad/l0MYt5jPR6QX5pnqM/giphy.gif',
-  `https://media.giphy.com/media/${'a'.repeat(257)}/l0MYt5jPR6QX5pnqM/giphy.gif`,
-  'https://i.giphy.com/path/l0MYt5jPR6QX5pnqM.gif',
-  'https://i.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif/',
-  'https://giphy.com/embed/l0MYt5jPR6QX5pnqM//',
   'https://giphy.com/embed/l0MYt5jPR6QX5pnqM/extra',
-  'https://giphy.com/gifs/reaction-%2el0MYt5jPR6QX5pnqM',
+  'https://tenor.com/view/reaction-gif-123456',
   'https://media2.tenor.com/AbCdEfGhIjK/tenor.gif',
-  'https://media1.tenor.com/images/not-a-hex-identifier-000000000000/tenor.gif',
-  'https://media1.tenor.com/images/1169d1ab96669e13062c1b23ce5b9b0/tenor.gif',
-  'https://media1.tenor.com/images/1169d1ab96669e13062c1b23ce5b9b01/tenor.svg',
-  'https://media1.tenor.com/images/1169d1ab96669e13062c1b23ce5b9b01/variant/tenor.gif',
   'https://media.tenor.com/a/tenor.gif',
   'https://media.tenor.com/AbCdEfGhIjK/.gif',
   'https://media.tenor.com/AbCdEfGhIjK/a..b.gif',
   'https://media.tenor.com/AbCdEfGhIjK/reaction.svg',
-  'https://media.tenor.com/m/AbCdEfGhIjK/too/many/segments/tenor.gif',
-  'https://media.tenor.com/m/AbCdEfGhIjK/variant-with-more-than-thirty-two-characters/tenor.gif',
-  'https://media.tenor.com/AbCdEfGhIjK/tenor.gif/',
-  'https://media.tenor.com/AbCdEfGhIjK//tenor.gif',
-  'https://media.tenor.com/./AbCdEfGhIjK/tenor.gif',
-  'https://media.tenor.com/AbCdEfGhIjK/../AbCdEfGhIjK/tenor.gif',
-  'https://tenor.com/view/reaction-gif-123456',
+  'https://static.klipy.com/ii/4493325008d34b7bf8cd6813cd5c1619/12/66/VRmb0agTs8UFUzia.svg',
+  'https://static.klipy.com/ii/not-a-hash/12/66/VRmb0agTs8UFUzia.gif',
+  'https://static.klipy.com/ii/4493325008d34b7bf8cd6813cd5c1619/1/66/VRmb0agTs8UFUzia.gif',
+  'https://static.klipy.com/ii/4493325008d34b7bf8cd6813cd5c1619/12/666/VRmb0agTs8UFUzia.gif',
+  'https://static.klipy.com/ii/4493325008d34b7bf8cd6813cd5c1619/12/66/a..b.gif',
+  'https://static.klipy.com.evil.example/ii/4493325008d34b7bf8cd6813cd5c1619/12/66/file.gif',
+  'https://cdn.klipy.com/ii/4493325008d34b7bf8cd6813cd5c1619/12/66/file.gif',
   'https://example.com/reaction.gif',
-  'https://giphy.com\\@evil.example/embed/l0MYt5jPR6QX5pnqM',
-  `https://giphy.com/embed/l0MYt5jPR6QX5pnqM?${'a'.repeat(2_048)}`,
   ' javascript:alert(1)',
   'javascript:alert(1)'
 ] as const;
@@ -190,12 +144,16 @@ describe('parseExternalGifUrl', () => {
     expect(parseExternalGifUrl(url)).toBeNull();
   });
 
-  it('removes fragments from canonical and media URLs', () => {
+  it('removes URL fragments without rewriting provider query parameters', () => {
     expect(
-      parseExternalGifUrl('https://media.tenor.com/AbCdEfGhIjK/tenor.gif#fragment')
+      parseExternalGifUrl(
+        'https://static.klipy.com/ii/4493325008d34b7bf8cd6813cd5c1619/12/66/VRmb0agTs8UFUzia.gif?x=1#fragment'
+      )
     ).toMatchObject({
-      canonicalUrl: 'https://media.tenor.com/AbCdEfGhIjK/tenor.gif',
-      resourceUrl: 'https://media.tenor.com/AbCdEfGhIjK/tenor.gif'
+      canonicalUrl:
+        'https://static.klipy.com/ii/4493325008d34b7bf8cd6813cd5c1619/12/66/VRmb0agTs8UFUzia.gif?x=1',
+      resourceUrl:
+        'https://static.klipy.com/ii/4493325008d34b7bf8cd6813cd5c1619/12/66/VRmb0agTs8UFUzia.gif?x=1'
     });
   });
 });
@@ -203,8 +161,10 @@ describe('parseExternalGifUrl', () => {
 describe('parseExternalGifMessageBody', () => {
   it('accepts a standalone supported URL with surrounding whitespace', () => {
     expect(
-      parseExternalGifMessageBody('  https://media.tenor.com/AbCdEfGhIjK/tenor.gif\n')
-    ).not.toBeNull();
+      parseExternalGifMessageBody(
+        '  https://static.klipy.com/ii/4493325008d34b7bf8cd6813cd5c1619/12/66/VRmb0agTs8UFUzia.gif\n'
+      )
+    ).toMatchObject({ provider: 'klipy' });
   });
 
   it.each([
@@ -219,7 +179,8 @@ describe('parseExternalGifMessageBody', () => {
 });
 
 describe('resolveExternalGifMessage', () => {
-  const body = 'https://media.tenor.com/AbCdEfGhIjK/excited-happy-dance.gif';
+  const body =
+    'https://static.klipy.com/ii/4493325008d34b7bf8cd6813cd5c1619/12/66/VRmb0agTs8UFUzia.gif';
 
   it('requires the server capability', () => {
     expect(
@@ -245,7 +206,7 @@ describe('resolveExternalGifMessage', () => {
         supportsCapability: true,
         hasPersistedLinkPreview: false
       })
-    ).toMatchObject({ provider: 'tenor', renderMode: 'image' });
+    ).toMatchObject({ provider: 'klipy', renderMode: 'image' });
   });
 });
 
